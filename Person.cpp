1074b019bb1e70a7d31c42f1ec2d3d6eaa814e37
jwdichter <justin.dichter@richmond.edu>
2018-10-24 09:58:09 -0400

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -11,11 +11,35 @@ Person::Person()
   dateOfBirth.tm_year = 0;
 }
 
-Person::Person(const Person& other) {}
+Person::Person(const Person& other) {
+  URID = other.URID;
+  netID = other.netID;
+  lastName = other.lastName;
+  firstName = other.firstName;
+  eMail = other.eMail;
+  Address = other.Address;
+  Phone = other.Phone;
+  
+  dateOfBirth.tm_mon = other.dateOfBirth.tm_mon;
+  dateOfBirth.tm_day = other.dateOfBirth.tm_day;
+  dateOfBirth.tm_year = other.dateOfBirth.tm_year;
+}
 
 Person::Person(int urid, std::string netid, std::string lname, std::string fname,
                int dob_day, int dob_month, int dob_year,
-               std::string email, std::string address, long phone) {}
+               std::string email, std::string address, long phone) {
+  URID = urid;
+  netID = netid;
+  lastName = lname;
+  firstName = fname;
+  eMail = email;
+  Address = address;
+  Phone = phone;
+  
+  dateOfBirth.tm_mon = dob_month;
+  dateOfBirth.tm_day = dob_day;
+  dateOfBirth.tm_year = dob_year;
+}
 
 Person::~Person() {}
 
@@ -28,13 +52,17 @@ int          Person::getURID() {return URID;}
 std::string  Person::getEmail() {return eMail;}
 long         Person::getPhone() {return Phone;}
 
-void Person::setFirstName(std::string fname) {}
+void Person::setFirstName(std::string fname) {firstName = fname;}
 void Person::setLastName(std::string lname) {lastName = lname;}
-void Person::setDateOfBirth(int day, int month, int year) {}
+void Person::setDateOfBirth(int day, int month, int year) {
+        dateOfBirth.tm_day = day;
+        dateOfBirth.tm_month = month-1;
+        dateOfBirth.tm_year = year-1900;
+}
 void Person::setAddress(std::string address) {Address = address;}
-void Person::setNetID(std::string netid) {}
+void Person::setNetID(std::string netid) {netID = netid;}
 void Person::setURID(int urid)  {URID = urid;}
-void Person::setEmail(std::string email) {}
+void Person::setEmail(std::string email) {eMail = email;}
 void Person::setPhone(long number) {Phone = number;}
 
 
