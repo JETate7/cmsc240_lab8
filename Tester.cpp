5a9025aa71511f39efcc08a74cd1f259b6062aba
jwdichter <justin.dichter@richmond.edu>
2018-10-28 19:00:37 -0400

    Update Tester.cpp

--- Tester.cpp
+++ Tester.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include "Person.h"
+#include "Student.h"
 
 int main() {
+	cout << "----------PERSON----------" << endl;
+	
 	cout << "Testing constructors put/getters: " << endl;
 	cout << "-------------------------" << endl;
 
 	cout << "Person p;" << endl;
 	Person p;
+	
+	cout << "p.setURID(12341234);" << endl;
+	p.setURID(12341234);
+	cout <<"p.getURID()" << endl
+	cout << p.getURID() << endl;
+	
+	cout << "p.setNetID(\"jd2jd\");" << endl;
+	p.setFirstName("jd2jd");
+	cout <<"p.getNetID()" << endl
+	cout << p.getNetID() << endl;
 
+	cout << "p.setFirstName(\"John\");" << endl;
+	p.setFirstName("John");
+	cout <<"p.getFirstName()" << endl
+	cout << p.getFirstName() << endl;
+	
 	cout << "p.setLastName(\"Doe\");" << endl;
 	p.setLastName("Doe");
 	cout <<"p.getLastName()" << endl
@@ -17,6 +35,11 @@ int main() {
 	p.setAddress("321 Lane Road");
 	cout << "p.getAddress()" << endl;
 	cout << p.getAddress() << endl;
+	
+	cout << "p.setDateOfBirth(12, 25, 1990);" << endl;
+	p.setFirstName(12, 25, 1990);
+	cout <<"p.getDateOfBirth()" << endl
+	cout << p.getDateOfBirth() << endl;
 
 	cout << "Testing overwrite: " << endl;
 	cout << "-------------------------" << endl;
