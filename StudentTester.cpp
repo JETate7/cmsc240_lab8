c78f53095457c9b925a8b294576323b9e3ef957a
jwdichter <justin.dichter@richmond.edu>
2018-10-29 17:28:28 -0400

    Update StudentTester.cpp

--- StudentTester.cpp
+++ StudentTester.cpp
@@ -1,104 +1,106 @@
-#include <iostream>
+include <iostream>
 #include "Person.h"
 #include "Student.h"
+#include <list>
+#include <string>
 
 using namespace std;
 
 int main() {
-	cout << "----------STUDENT----------" << endl;
-	
-	cout << "Testing constructors put/getters: " << endl;
-	cout << "-------------------------" << endl;
+        cout << "----------STUDENT----------" << endl;
 
-	cout << "Student s;" << endl;
-	Student s;
-	
-	cout << "s.setURID(12341234);" << endl;
-	s.setURID(12341234);
-	cout << "URID: " << s.getURID() << " [12341234]" << endl;
-	
-	cout << "s.setNetID(\"jd2jd\");" << endl;
-	s.setNetID("jd2jd");
-	cout << "NetID: " << s.getNetID() << " [jd2jd]" << endl;
+        cout << "Testing constructors put/getters: " << endl;
+        cout << "-------------------------" << endl;
 
-	cout << "s.setFirstName(\"John\");" << endl;
-	s.setFirstName("John");
-	cout << "First Name: " << s.getFirstName() << " [John]" << endl;
-	
-	cout << "s.setLastName(\"Doe\");" << endl;
-	s.setLastName("Doe");
-	cout << "Last Name: " << s.getLastName() << " [Doe]" << endl;
+        cout << "Student s;" << endl;
+        Student s;
 
-	cout << "s.setAddress(\"321 Lane Road\");" << endl;
-	s.setAddress("321 Lane Road");
-	cout << "Address: " << s.getAddress() << " [321 Lane Road]" << endl;
-	
-	cout << "s.setDateOfBirth(25, 12, 1990);" << endl;
-	s.setDateOfBirth(25, 12, 1990);
-	cout << "DateOfBirth (day): " << s.getDateOfBirth().tm_mday << " [25]" << endl;
-	cout << "DateOfBirth (month): " << s.getDateOfBirth().tm_mon <<  " [11]" << endl;
-	cout << "DateOfBirth (year): " << s.getDateOfBirth().tm_year << " [90]" <<endl;
+        cout << "s.setURID(12341234);" << endl;
+        s.setURID(12341234);
+        cout << "URID: " << s.getURID() << " [12341234]" << endl;
 
-	cout << "s.setPhone(2014822829);" << endl;
-	s.setPhone(2014822829);
-	cout <<"s.getPhone()" << endl;
-	cout << s.getPhone() << endl;
-	
-	cout << "s.setAdmitDate(8, 5, 2016);" << endl;
-	s.setAdmitDate(8, 5, 2016);
-	cout << "Admit Date (day): " << s.getAdmitDate().tm_mday << " [8]" << endl;
-	cout << "Admit Date (month): " << s.getAdmitDate().tm_mon << " [4]" << endl;
-	cout << "Admit Date (year): " << s.getAdmitDate().tm_year << " [116]" << endl;
+        cout << "s.setNetID(\"jd2jd\");" << endl;
+        s.setNetID("jd2jd");
+        cout << "NetID: " << s.getNetID() << " [jd2jd]" << endl;
+
+        cout << "s.setFirstName(\"John\");" << endl;
+        s.setFirstName("John");
+        cout << "First Name: " << s.getFirstName() << " [John]" << endl;
+
+        cout << "s.setLastName(\"Doe\");" << endl;
+        s.setLastName("Doe");
+        cout << "Last Name: " << s.getLastName() << " [Doe]" << endl;
+
+        cout << "s.setAddress(\"321 Lane Road\");" << endl;
+        s.setAddress("321 Lane Road");
+        cout << "Address: " << s.getAddress() << " [321 Lane Road]" << endl;
+
+        cout << "s.setDateOfBirth(25, 12, 1990);" << endl;
+        s.setDateOfBirth(25, 12, 1990);
+        cout << "DateOfBirth (day): " << s.getDateOfBirth().tm_mday << " [25]" << endl;
+        cout << "DateOfBirth (month): " << s.getDateOfBirth().tm_mon <<  " [11]" << endl;
+        cout << "DateOfBirth (year): " << s.getDateOfBirth().tm_year << " [90]" <<endl;
+
+        cout << "s.setPhone(2014822829);" << endl;
+        s.setPhone(2014822829);
+        cout <<"s.getPhone()" << endl;
+        cout << s.getPhone() << endl;
+
+        cout << "s.setAdmitDate(8, 5, 2016);" << endl;
+        s.setAdmitDate(8, 5, 2016);
+        cout << "Admit Date (day): " << s.getAdmitDate().tm_mday << " [8]" << endl;
+        cout << "Admit Date (month): " << s.getAdmitDate().tm_mon << " [4]" << endl;
+        cout << "Admit Date (year): " << s.getAdmitDate().tm_year << " [116]" << endl;
  
 	cout << "s.setSchool(Student::LAW);" << endl;
-	s.setSchool(Student::LAW);
-	cout << "School: " << s.getSchool() << " [LAW]" << endl;
-	
-	cout << "s.setGPA(3.21);" << endl;
-	s.setGPA(3.21);
-	cout << "GPA: " << s.getGPA() << " [3.21]" << endl;
-	
-	cout << "s.setUnitsCompleted(8.0);" << endl;
-	s.setUnitsCompleted(8.0);
-	cout << "Completed Units: " << s.getUnitsCompleted() << " [8.0]" << endl;
-	
-	cout << "s.setFullTimeStatus(false);" << endl;
-	s.setFullTimeStatus(false);
-	cout << "Full Time Status: " << s.isFullTime() << " [false]" << endl;
+        s.setSchool(Student::LAW);
+        cout << "School: " << s.getSchool() << " [LAW]" << endl;
 
-	cout << "-------------------------" << endl;
-  	cout << "Testing Course methods: " << endl;
-	cout << "-------------------------" << endl;
-  
-  	cout << "s.addCourse(\"Math\");" << endl;
-	cout << "s.addCourse(\"Biology\");" << endl;
-	cout << "s.addCourse(\"Spanish\");" << endl;
-	s.addCourse("Math");
-	s.addCourse("Biology");
-	s.addCourse("Spanish");
-	
-	cout << "s.printCourses();" << endl;
-	s.printCourses();
-	
-	cout << "s.removeCourse(\"Spanish\");" << endl;
-	s.removeCourse("Spanish");
-	
-	cout << "s.clearCourses();" << endl;
-	s.clearCourses();
-	
-	cout << "Setting courses from a new course list and printing" << endl;
-	list<string> newCourses();
-	newCourses.push_front("French");
-	newCourses.push_front("Chemistry");
-	cout <<"s.setCourses(newCourses);" << endl;
-	s.setCourses(newCourses);
-	s.printCourses();
-	
+        cout << "s.setGPA(3.21);" << endl;
+        s.setGPA(3.21);
+        cout << "GPA: " << s.getGPA() << " [3.21]" << endl;
+
+        cout << "s.setUnitsCompleted(8.0);" << endl;
+        s.setUnitsCompleted(8.0);
+        cout << "Completed Units: " << s.getUnitsCompleted() << " [8.0]" << endl;
+
+        cout << "s.setFullTimeStatus(false);" << endl;
+        s.setFullTimeStatus(false);
+        cout << "Full Time Status: " << s.isFullTime() << " [false]" << endl;
+
+        cout << "-------------------------" << endl;
+        cout << "Testing Course methods: " << endl;
+        cout << "-------------------------" << endl;
+
+        cout << "s.addCourse(\"Math\");" << endl;
+        cout << "s.addCourse(\"Biology\");" << endl;
+        cout << "s.addCourse(\"Spanish\");" << endl;
+        s.addCourse("Math");
+        s.addCourse("Biology");
+        s.addCourse("Spanish");
+
+        cout << "s.printCourses();" << endl;
+        s.printCourses();
+
+        cout << "s.removeCourse(\"Spanish\");" << endl;
+        s.removeCourse("Spanish");
+
+        cout << "s.clearCourses();" << endl;
+        s.clearCourses();
+
+        cout << "Setting courses from a new course list and printing" << endl;
+        list<string> newCourses;
+        newCourses.push_front("French");
+        newCourses.push_front("Chemistry");
+        cout <<"s.setCourses(newCourses);" << endl;
+        s.setCourses(newCourses);
+        s.printCourses();
+
         cout << "-------------------------" << endl;
         cout << "Testing overwrite: " << endl;
         cout << "-------------------------" << endl;
-	
-	cout << "s.setURID(56781234);" << endl;
+
+   cout << "s.setURID(56781234);" << endl;
         s.setURID(56781234);
         cout << "URID: " << s.getURID() << " [56781234]" << endl;
 
@@ -127,39 +129,39 @@ int main() {
         cout << "s.setPhone(213434534);" << endl;
         s.setPhone(213434534);
         cout <<"Phone: " << s.getPhone() << " [213434534]" << endl;
-	
-	cout << "s.setAdmitDate(1, 4, 2014);" << endl;
-	s.setAdmitDate(1, 4, 2014);
-	cout << "Admit Date (day): " << s.getAdmitDate().tm_mday << " [1]" << endl;
-	cout << "Admit Date (month): " << s.getAdmitDate().tm_mon << " [3]" << endl;
-	cout << "Admit Date (year): " << s.getAdmitDate().tm_year << " [114]" << endl;
- 
-	cout << "s.setSchool(Student::RSB);" << endl;
-	s.setSchool(Student::RSB);
-	cout << "School: " << s.getSchool() << " [RSB]" << endl;
-	
-	cout << "s.setGPA(3.12);" << endl;
-	s.setGPA(3.12);
-	cout << "GPA: " << s.getGPA() << " [3.12]" << endl;
-	
-	cout << "s.setUnitsCompleted(9.0);" << endl;
-	s.setUnitsCompleted(9.0);
-	cout << "Completed Units: " << s.getUnitsCompleted() << " [9.0]" << endl;
-	
-	cout << "s.setFullTimeStatus(true);" << endl;
-	s.setFullTimeStatus(true);
-	cout << "Full Time Status: " << s.isFullTime() << " [true]" << endl;
 
+        cout << "s.setAdmitDate(1, 4, 2014);" << endl;
+        s.setAdmitDate(1, 4, 2014);
+        cout << "Admit Date (day): " << s.getAdmitDate().tm_mday << " [1]" << endl;
+        cout << "Admit Date (month): " << s.getAdmitDate().tm_mon << " [3]" << endl;
+        cout << "Admit Date (year): " << s.getAdmitDate().tm_year << " [114]" << endl;
+
+        cout << "s.setSchool(Student::RSB);" << endl;
+        s.setSchool(Student::RSB);
+        cout << "School: " << s.getSchool() << " [RSB]" << endl;
+
+        cout << "s.setGPA(3.12);" << endl;
+        s.setGPA(3.12);
+        cout << "GPA: " << s.getGPA() << " [3.12]" << endl;
+
+        cout << "s.setUnitsCompleted(9.0);" << endl;
+        s.setUnitsCompleted(9.0);
+        cout << "Completed Units: " << s.getUnitsCompleted() << " [9.0]" << endl;
+
+        cout << "s.setFullTimeStatus(true);" << endl;
+        s.setFullTimeStatus(true);
+        cout << "Full Time Status: " << s.isFullTime() << " [true]" << endl;
+
+         cout << "-------------------------" << endl;
+        cout << "Testing copy constructor: " << endl;
         cout << "-------------------------" << endl;
-	cout << "Testing copy constructor: " << endl;
-	cout << "-------------------------" << endl;
-	
-	cout << "Student s1(s);" << endl;
-	Student s1(s);
 
-	cout << "Testing getters on s1: " << endl;
+        cout << "Student s1(s);" << endl;
+        Student s1(s);
 
-	cout << "URID: " << s1.getURID() << " [12341234]" << endl;
+        cout << "Testing getters on s1: " << endl;
+
+        cout << "URID: " << s1.getURID() << " [12341234]" << endl;
 
         cout << "NetID: " << s1.getNetID() << " [jd2jd]" << endl;
 
@@ -175,31 +177,32 @@ int main() {
 
         cout <<"s1.getPhone()" << endl;
         cout << s1.getPhone() << endl;
+
+        cout << "Admit Date (day): " << s1.getAdmitDate().tm_mday << " [8]" << endl;
+        cout << "Admit Date (month): " << s1.getAdmitDate().tm_mon << " [4]" << endl;
+        cout << "Admit Date (year): " << s1.getAdmitDate().tm_year << " [116]" << endl;
+
+        cout << "School: " << s1.getSchool() << " []" << endl;
+
+        cout << "GPA: " << s1.getGPA() << " [3.21]" << endl;
+
+        cout << "Completed Units: " << s1.getUnitsCompleted() << " [8.0]" << endl;
+
+        cout << "Full Time Status: " << s1.isFullTime() << " [false]" << endl;
+
+        cout << "-------------------------" << endl;
+        cout << "Testing detailed constructor: " << endl;
+        cout << "-------------------------" << endl;
+
+        newCourses.clear();
+        newCourses.push_front("Art");
+        newCourses.push_front("Math");
+
+        cout << "Student s2(11111111, \"ab1cd\", \"last\", \"first\", 31, 10, 2018, \"abcd@richmond.edu\", \"123 happy lane\", 1234567890, 15, 8, 2015, Student::LAW, true, 10.5, 3.45, newCourses);" << endl;
+        Student s2(11111111, "ab1cd", "last", "first", 31, 10, 2018, "abcd@richmond.edu", "123 happy lane", 1234567890, 15, 8, 2015, Student::LAW, true, 10.5, 3.45, newCourses);
+, "123 happy lane", 1234567890, 15, 8, 2015, Student::LAW, true, 10.5, 3.45, newCourses);
 	
-	cout << "Admit Date (day): " << s1.getAdmitDate().tm_mday << " [8]" << endl;
-	cout << "Admit Date (month): " << s1.getAdmitDate().tm_mon << " [4]" << endl;
-	cout << "Admit Date (year): " << s1.getAdmitDate().tm_year << " [116]" << endl;
-	
-	cout << "School: " << s1.getSchool() << " []" << endl;
-
-	cout << "GPA: " << s1.getGPA() << " [3.21]" << endl;
-
-	cout << "Completed Units: " << s1.getUnitsCompleted() << " [8.0]" << endl;
-
-	cout << "Full Time Status: " << s1.isFullTime() << " [false]" << endl;
-
-	cout << "-------------------------" << endl;
-	cout << "Testing detailed constructor: " << endl;
-	cout << "-------------------------" << endl;
-
-	newCourses.clear();
-	newCourses.push_front("Art");
-	newCourses.push_front("Math");
-	
-	cout << "Student s2(11111111, \"ab1cd\", \"last\", \"first\", 31, 10, 2018, \"abcd@richmond.edu\", \"123 happy lane\", 1234567890, 15, 8, 2015, Student::LAW, true, 10.5, 3.45, newCourses);" << endl;
-	Student s2(11111111, "ab1cd", "last", "first", 31, 10, 2018, "abcd@richmond.edu", "123 happy lane", 1234567890, 15, 8, 2015, Student::LAW, true, 10.5, 3.45, newCourses);
-	
-	cout << "Testing getters on s2: " << endl;
+      cout << "Testing getters on s2: " << endl;
 
         cout << "URID: " << s2.getURID() << " [11111111]" << endl;
 
@@ -214,21 +217,21 @@ int main() {
         cout << "DateOfBirth (day): " << s2.getDateOfBirth().tm_mday << " [31]" << endl;
         cout << "DateOfBirth (month): " << s2.getDateOfBirth().tm_mon << " [9] " << endl;
         cout << "DateOfBirth (year): " << s2.getDateOfBirth().tm_year << " [118] " <<endl;
-	cout << "Email: " << s2.getEmail() << " [abcd@richmond.edu]" << endl;
+        cout << "Email: " << s2.getEmail() << " [abcd@richmond.edu]" << endl;
 
         cout << "Phone: " << s2.getPhone() << " [1234567890]" << endl;
-	
-	cout << "Admit Date (day): " << s2.getAdmitDate().tm_mday << " [15]" << endl;
-	cout << "Admit Date (month): " << s2.getAdmitDate().tm_mon << " [7]" << endl;
-	cout << "Admit Date (year): " << s2.getAdmitDate().tm_year << " [115]" << endl;
-	
-	cout << "School: " << s2.getSchool() << " [LAW]" << endl;
 
-	cout << "GPA: " << s2.getGPA() << " [3.45]" << endl;
+        cout << "Admit Date (day): " << s2.getAdmitDate().tm_mday << " [15]" << endl;
+        cout << "Admit Date (month): " << s2.getAdmitDate().tm_mon << " [7]" << endl;
+        cout << "Admit Date (year): " << s2.getAdmitDate().tm_year << " [115]" << endl;
 
-	cout << "Completed Units: " << s2.getUnitsCompleted() << " [10.5]" << endl;
+        cout << "School: " << s2.getSchool() << " [LAW]" << endl;
 
-	cout << "Full Time Status: " << s2.isFullTime() << " [true]" << endl;
-	
-	return 0;
+        cout << "GPA: " << s2.getGPA() << " [3.45]" << endl;
+
+        cout << "Completed Units: " << s2.getUnitsCompleted() << " [10.5]" << endl;
+
+        cout << "Full Time Status: " << s2.isFullTime() << " [true]" << endl;
+
+        return 0;
 }
