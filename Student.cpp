6726931f5a8e0a6771c94edd894407a8aae6dce1
JETate7 <43750525+JETate7@users.noreply.github.com>
2018-10-29 00:21:23 -0400

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,58 +1,62 @@
 #ifndef __STUDENT_CPP__
-#define __STUDENT_CPP__
+#define __STUDENT_CPP__ 
+ 
+#include <iostream> 
+#include <list> 
+#include "Person.h" 
+#include "Student.h" 
+ 
+Student::Student() : Person() { 
+        School school; 
+        sch = school;  
+        fullTime = false;  
+        completeUnits = 0.0;  
+        GPA = 0.0;  
+        std::list<std::string> listCourse; 
+        courses = listCourse; 
+}                      // explicitly call Person() on implementation 
+Student::Student(const Student& other) : Person(other) {}  // call Person(other) on implementation 
+Student::Student(int urid, std::string netid, std::string lname, std::string fname,  int dob_day, int dob_mo, int dob_yr, std::string email, std::string address, long phone, int day_admit, int month_admit, int year_admit, School school, bool is_full_time, double units_completed, double grade, std::list<std::string> courseList) : Person(urid, netid, lname, fname, dob_day, dob_mo, dob_yr, email, address, phone){ 
+        dateOfAdmit.tm_mday = day_admit; 
+        dateOfAdmit.tm_mon = month_admit; 
+        dateOfAdmit.tm_year = year_admit; 
+        sch = school; 
+        fullTime = is_full_time; 
+        completeUnits = units_completed; 
+        GPA = grade; 
+        courses = courseList; 
+} 
+Student::~Student() {} 
+ 
+std::list<std::string> Student::getCourses() {return courses;} 
+void Student::addCourse(std::string course) {courses.push_back(course);} 
+void Student::removeCourse(std::string course) {courses.remove(course);} 
+void Student::printCourses() { 
+    std::cout << "Courses: ";  
+    for (std::list<std::string>::iterator it=courses.begin(); it!=courses.end(); it++) { 
+       std::cout << *it << ", "; 
+    } 
+    std::cout << std::endl; 
+} 
 
-#include "Person.h"
-#include "Student.h"
-
-Student::Student() : Person() {sch = null; fullTime = false; completeUnits = 0.0; GPA = 0.0; 
-	std::list<std::string> listCourse;
-	courses = listCourse;
-}                      // explicitly call Person() on implementation
-Student::Student(const Student& other) : Person(other) {}  // call Person(other) on implementation
-Student::Student(int urid, std::string netid, std::string lname, std::string fname, 
-               int dob_day, int dob_mo, int dob_yr, 
-               std::string email, std::string address, long phone,
-               int day_admit, int month_admit, int year_admit,
-               School school, bool is_full_time, 
-               double units_completed, double grade, std::list<std::string> courseList) : Person(urid, netid, lname, fname, dob_day, dob_month, dob_year, email, address, phone){
-	dateOfAdmit.tm_mday = day_admit;
-	dateOfAdmit.tm_mon = month_admit;
-	dateOfAdmit.tm_year = year_admit;
-	sch = school;
-	fullTime = is_full_time;
-	completeUnits = units_completed;
-	GPA = grade;
-	courses = courseList;
-}
-Student::~Student() {}
-
-std::list<std::string> Student::getCourses() {return courses;}
-void Student::addCourse(std::string course) {courses.insert(course);}
-void Student::removeCourse(std::string course) {courses.remove(course);}
-void Student::printCourses() {
-    cout << "Courses: "; 
-    for (std::list<std::string>::iterator it=courses.begin(); it!=courses.end(); it++) {
-       cout << *i << ", ";
-    }
-    cout << endl;
-}
 void Student::setCourses(std::list<std::string> newCourses) {courses = newCourses;}
 void Student::clearCourses() {courses.clear();}
 
 struct tm Student::getAdmitDate() {return dateOfAdmit;}
-School Student::getSchool() {return sch}
-double Student::getGPA() {return gpa;}
-double Student::getUnitsCompleted() {return completeUnits}
+Student::School Student::getSchool() {return sch;}
+double Student::getGPA() {return GPA;}
+double Student::getUnitsCompleted() {return completeUnits;}
 bool Student::isFullTime() { return fullTime; }
 
 void Student::setAdmitDate(int day, int month, int year) {
-	dateOfAdmit.tm_mday = day;
-	dateOfAdmit.tm_mon = month;
-	dateOfAdmit.tm_year = year;
+        dateOfAdmit.tm_mday = day;
+        dateOfAdmit.tm_mon = month;
+        dateOfAdmit.tm_year = year;
 }
 void Student::setSchool(School school) {sch = school;}
 void Student::setGPA(double gpa) {GPA = gpa;}
 void Student::setUnitsCompleted(double units) {completeUnits = units;}
 void Student::setFullTimeStatus(bool isFullTime) {fullTime = isFullTime;}
 
-#endif
+#endif 
+
